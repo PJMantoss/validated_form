2b67e9cb6566e210db6bc8598d06e85eb8514b4f
PJMantoss <joelptoss@gmail.com>
2019-11-09 14:55:49 +0100

    Add state in Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
 
+import Input from './Input';
+import CheckBox from './CheckBox';
+import Select from './Select';
+import TextField from './TextField';
+import Button from './Button';
+
 export class Form extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            newUser: {
+                name: '',
+                age: '',
+                gender: '',
+                skills: [],
+                about: ''
+            },
+            genderOptions: ['Male', 'Female', 'Others'],
+            skillOptions: ['Programming', 'Development', 'Design', 'Testing']
+        }
+    }
+
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <form>
+                <Input />
+                <Input />
+                <Select />
+                <CheckBox />
+                <TextField />
+                <Button />
+                <Button />
+            </form>
         )
     }
 }
